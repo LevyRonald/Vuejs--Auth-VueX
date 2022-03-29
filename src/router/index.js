2c1351182a7e31b285d38c36d7b71fcec6520051
Levy <levyronald3@gmail.com>
2022-03-29 10:52:29 -0300

    update: curso VueX aula 01 (completo)

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Gerentes from '../views/Gerentes.vue'
+import Home from '../views/Home.vue'
+import NovoUsuario from '../views/NovoUsuario'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/gerentes',
+    name: 'gerentes',
+    component: Gerentes
+  },
+  {
+    path: '/cadastre-se',
+    name: 'novo.usuario',
+    component: NovoUsuario
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
